ab3131529ecbcda16b13ba132e8ab75dc8c579b5
Glenn Ferrie <glenn@gfdata.io>
2022-06-06 21:00:31 -0400

    new schema generator agent

---
+++ bc.speedy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace bc.speedy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public sealed class SchemaGenerator
+    {
+        public SchemaSettings Settings { get; set; } = null;
+        public SchemaGenerator()
+        {
+            Settings = new SchemaSettings();
+        }
+    }
+    
+    public sealed class SchemaSettings
+    {
+        public SchemaSettings()
+        {
+            // TODO: get valus from config file. 
+        }
+
+        public string TableOwner { get; set; } = "dbo";
+        public bool AlwaysBracketIdentifiers { get; set; } = true;
+    }
+}
